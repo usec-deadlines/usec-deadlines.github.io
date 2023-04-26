63c936160720b7b1ba97e2b911b1505468f870a3
Nathan Malkin <nmalkin@cs.berkeley.edu>
2023-04-25 23:41:13 -0700

    Restore checkboxes being selected on load
    This reverts commit a0794395c0441a4714f8ced6c6b12728dd9f8627.
    
    The change was made upstream to:
    "change default behaviour of tag checkboxes: none checked instead of all checked"
    
    However, this fork already de-selected most checkboxes in favor of the usec one;
    this change breaks that behavior.
    
    (It also breaks the local storage of the selected checkboxes, which seems undesirable?)
    
    This commit restores the old behavior.

--- static/js/main.js
+++ static/js/main.js
@@ -119,8 +119,8 @@ $(function() {
     tags = default_tags;
   }
   for (var i = 0; i < tags.length; i++) {
-    $('#' + tags[i] + '-checkbox').prop('checked', false);
-    toggle_status[tags[i]] = false;
+    $('#' + tags[i] + '-checkbox').prop('checked', true);
+    toggle_status[tags[i]] = true;
   }
   store.set('{{ site.domain }}', tags);
 
